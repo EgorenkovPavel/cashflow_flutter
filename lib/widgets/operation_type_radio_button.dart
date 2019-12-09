688a2f23b4896f3d64c64e2a947b9c24b77cd6d7
Pavel <epipasha@gmail.com>
2019-12-09 20:16:14 +0300

    Change operation type button from radio to toggle

--- lib/widgets/operation_type_radio_button.dart
+++ lib/widgets/operation_type_radio_button.dart
@@ -10,23 +10,6 @@ class OperationTypeRadioButton extends StatelessWidget{
 
   const OperationTypeRadioButton({Key key, this.onChange, this.type, this.items}) : super(key: key);
 
-  Widget RadioButton(BuildContext context, OperationType _type, String title){
-    final color = type == _type ? Theme.of(context).primaryColor : Colors.transparent;
-    final textColor = type == _type ? Colors.white : Colors.black;
-
-    return FlatButton(
-      child: Text(title),
-      color: color,
-      //shape: Border.all(color: color),
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-      textColor: textColor,
-
-      onPressed: (){
-        onChange(_type);
-      },
-    );
-  }
-
   String getTitle(OperationType _type){
     switch(_type){
       case OperationType.INPUT: return 'INPUT';
@@ -38,15 +21,17 @@ class OperationTypeRadioButton extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Theme.of(context).primaryColor),
-        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: items.map((item) => RadioButton(context, item, getTitle(item))).toList(),
-      ),
+    return ToggleButtons(
+      children: items.map((item) => Text(getTitle(item), style: TextStyle(color: item == type ? Colors.white : Colors.black),)).toList(),
+      borderColor: Theme.of(context).primaryColor,
+      borderWidth: 2.0,
+      selectedBorderColor:  Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.all(Radius.circular(4.0)),
+      fillColor: Theme.of(context).primaryColor,
+      isSelected: items.map((item) => item == type).toList(),
+      onPressed: (index){
+        onChange(items[index]);
+      },
     );
   }
 }
