742584a106aec494e842aa0702b4a6c387d4f401
Pavel <epipasha@gmail.com>
2020-02-26 21:10:35 +0300

    Change animation

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class Carousel<T> extends StatefulWidget {
+class Carousel<T> extends StatelessWidget {
   final double itemHeight;
 
   final List<T> items;
@@ -8,6 +10,7 @@ class Carousel<T> extends StatefulWidget {
   final ValueChanged<int> onPageChanged;
   final IndexedWidgetBuilder itemBuilder;
 
+  PageController _pageController;
 
   Carousel(
       {Key key,
@@ -19,34 +22,20 @@ class Carousel<T> extends StatefulWidget {
       : super(key: key);
 
   @override
-  _CarouselState createState() => _CarouselState();
-
-}
-
-class _CarouselState<T> extends State<Carousel<T>> {
-  PageController _pageController;
-
-
-  @override
-  void didChangeDependencies() {
-
-  }
-
-  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constrains) {
         _pageController = PageController(
-          initialPage: widget.items.indexOf(widget.initialItem),
+          initialPage: items.indexOf(initialItem),
           keepPage: true,
-          viewportFraction: 1 / (constrains.maxHeight / widget.itemHeight),
+          viewportFraction: 1 / (constrains.maxHeight / itemHeight),
         );
         return Stack(
           children: <Widget>[
             Center(
               child: Container(
                 margin: EdgeInsets.all(8.0),
-                height: widget.itemHeight - 16.0,
+                height: itemHeight - 16.0,
                 decoration: BoxDecoration(
                     border: Border.all(
                         width: 3.0, color: Theme.of(context).primaryColor),
@@ -54,45 +43,44 @@ class _CarouselState<T> extends State<Carousel<T>> {
               ),
             ),
             PageView.builder(
-              controller: _pageController,
-              scrollDirection: Axis.vertical,
-              itemCount: widget.items.length,
-              onPageChanged: widget.onPageChanged,
-              itemBuilder: (context, pos) {
-                return AnimatedBuilder(
-                  builder: (BuildContext context, Widget child) {
-                    if (!_pageController.position.haveDimensions) {
+                controller: _pageController,
+                scrollDirection: Axis.vertical,
+                itemCount: items.length,
+                onPageChanged: onPageChanged,
+                itemBuilder: (context, pos) {
+                  return AnimatedBuilder(
+                    builder: (BuildContext context, Widget child) {
+                      if (!_pageController.position.haveDimensions) {
+                        return Transform.scale(
+                          scale: 1 - (min((pos - _pageController.initialPage).abs(), 1)) / 10,
+                          child: child,
+                        );
+                      }
                       return Transform.scale(
-                        scale: 1 - (pos - _pageController.initialPage).abs() / 10,
+                        scale: 1 - (min((pos - _pageController.page).abs(), 1)) / 10,
                         child: child,
                       );
-                    }
-                    return Transform.scale(
-                      scale: 1 - (pos - _pageController.page).abs() / 10,
-                      child: child,
-                    );
-                  },
-                  animation: _pageController,
-                  child: GestureDetector(
-                    child: Container(
-                        margin: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0),
-                          border: Border.all(
-                              width: 1.0,
-                              color: Theme.of(context).primaryColor),
-                        ),
-                        height: 20.0,
-                        child: widget.itemBuilder(context, pos)),
-                    onTap: () {
-                      _pageController.animateToPage(pos,
-                          duration: Duration(seconds: 1),
-                          curve: ElasticOutCurve());
                     },
-                  ),
-                );
-              },
-            ),
+                    animation: _pageController,
+                    child: GestureDetector(
+                      child: Container(
+                          margin: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            border: Border.all(
+                                width: 1.0,
+                                color: Theme.of(context).primaryColor),
+                          ),
+                          height: 20.0,
+                          child: itemBuilder(context, pos)),
+                      onTap: () {
+                        _pageController.animateToPage(pos,
+                            duration: Duration(seconds: 1),
+                            curve: ElasticOutCurve());
+                      },
+                    ),
+                  );
+                }),
           ],
         );
       },
