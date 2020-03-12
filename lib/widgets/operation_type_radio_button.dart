129b8eef77cac20f5ba3f16015c3b93a5b77cae1
Pavel <epipasha@gmail.com>
2020-03-12 20:16:29 +0300

    Fixes

--- lib/widgets/operation_type_radio_button.dart
+++ lib/widgets/operation_type_radio_button.dart
@@ -1,7 +1,7 @@
 import 'package:cashflow/data/operation_type.dart';
 import 'package:flutter/material.dart';
 
-class OperationTypeRadioButton extends StatelessWidget {
+class OperationTypeRadioButton extends StatelessWidget{
   final Function onChange;
   final OperationType type;
 
@@ -13,34 +13,11 @@ class OperationTypeRadioButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-//    return ToggleButtons(
-//      children: items
-//          .map((item) => Container(
-//                padding: const EdgeInsets.all(8.0),
-//                alignment: Alignment.center,
-//                child: Text(
-//                  getOperationTitle(context, item).toUpperCase(),
-//                  style: TextStyle(
-//                      color: item == type ? Colors.white : Colors.black),
-//                ),
-//              ))
-//          .toList(),
-//      borderColor: Theme.of(context).primaryColor,
-//      borderWidth: 2.0,
-//      selectedBorderColor: Theme.of(context).primaryColor,
-//      borderRadius: BorderRadius.all(Radius.circular(4.0)),
-//      fillColor: Theme.of(context).primaryColor,
-//      isSelected: items.map((item) => item == type).toList(),
-//      onPressed: (index) {
-//        onChange(items[index]);
-//      },
-//    );
     return Row(
       children: items
           .map((item) => Flexible(
                 child: ClipRRect(
                   borderRadius: getBorderRadius(item),
-
                   child: RawMaterialButton(
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
