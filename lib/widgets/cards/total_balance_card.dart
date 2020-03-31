6c8d024dfd9bc2fb6e981bbb3fe8565b063fef08
Pavel <epipasha@gmail.com>
2020-03-31 11:38:17 +0300

    Add empty balance card

--- lib/widgets/cards/total_balance_card.dart
+++ lib/widgets/cards/total_balance_card.dart
@@ -1,5 +1,6 @@
 import 'package:cashflow/data/objects/account_balance.dart';
 import 'package:cashflow/widgets/card_title.dart';
+import 'package:cashflow/widgets/lists/account_list.dart';
 import 'package:flutter/material.dart';
 
 class TotalBalanceCard extends StatelessWidget {
@@ -13,40 +14,67 @@ class TotalBalanceCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16.0),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          CardTitle('Total balance'),
-          Container(
-            height: 200.0,
-            alignment: Alignment.center,
-            color: Colors.grey,
-            child: Text('Here will be a chart'),
-          ),
-          ExpansionTile(
-            title: Text(
-                accounts
-                    .map((account) => account.balance)
-                    .fold(0, (a, b) => a + b)
-                    .toString(),
-                style: Theme.of(context).textTheme.title),
-            children: accounts
-                .map((account) => Column(
-                      children: <Widget>[
-                        Divider(),
-                        ListTile(
-                          title: Text(account.title),
-                          trailing: Text(
-                            account.balance.toString(),
-                            style: Theme.of(context).textTheme.title,
-                          ),
+      child: body(context),
+    );
+  }
+
+  Widget body(BuildContext context) {
+    if(accounts.isEmpty){
+      return emptyBody(context);
+    }else{
+      return filledBody(context);
+    }
+  }
+
+  Widget filledBody(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        CardTitle('Total balance'),
+        Container(
+          height: 200.0,
+          alignment: Alignment.center,
+          color: Colors.grey,
+          child: Text('Here will be a chart'),
+        ),
+        ExpansionTile(
+          title: Text(
+              accounts
+                  .map((account) => account.balance)
+                  .fold(0, (a, b) => a + b)
+                  .toString(),
+              style: Theme.of(context).textTheme.title),
+          children: accounts
+              .map((account) => Column(
+                    children: <Widget>[
+                      Divider(),
+                      ListTile(
+                        title: Text(account.title),
+                        trailing: Text(
+                          account.balance.toString(),
+                          style: Theme.of(context).textTheme.title,
                         ),
-                      ],
-                    ))
-                .toList(),
-          ),
-        ],
-      ),
+                      ),
+                    ],
+                  ))
+              .toList(),
+        ),
+      ],
+    );
+  }
+
+  Widget emptyBody(BuildContext context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        CardTitle('No accounts'),
+        Align(
+          child: FlatButton(child: Text('Add account'),
+          onPressed: (){
+            AccountList.addItem(context);
+          },),
+        ),
+      ],
     );
   }
 }
