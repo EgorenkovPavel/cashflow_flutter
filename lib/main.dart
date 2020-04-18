9c483b851c56b41c4a2c57b7a579e713caa06927
Pavel <epipasha@gmail.com>
2020-04-18 09:54:06 +0300

    Upgrade packages

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ import 'data/repository.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
