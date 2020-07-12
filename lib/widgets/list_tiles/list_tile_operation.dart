6b863d67340b5a3162c15b560acba9603c74efda
Pavel <epipasha@gmail.com>
2020-07-12 12:19:19 +0300

    Fix

--- lib/widgets/list_tiles/list_tile_operation.dart
+++ lib/widgets/list_tiles/list_tile_operation.dart
@@ -12,53 +12,42 @@ class ListTileOperation extends StatelessWidget {
   const ListTileOperation(this._operation, {Key key, this.onTap})
       : super(key: key);
 
+  void onLongPress(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) => Wrap(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.delete),
+            title: Text('DELETE'),
+            onTap: () {
+              Provider.of<Repository>(context, listen: false)
+                  .deleteOperation(_operation);
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(_operation.account.title),
-      subtitle: _operation.type == OperationType.TRANSFER
+      subtitle: Text(_operation.account.title),
+      title: _operation.type == OperationType.TRANSFER
           ? Text(_operation.recAccount.title)
           : Text(_operation.category.title),
-      trailing: Column(
-        children: <Widget>[
-          Text(
-          NumberFormat().format(_operation.sum),
-            style: Theme.of(context).textTheme.headline,
-          ),
-          Text(
-            DateFormat.yMMMd(Localizations.localeOf(context).languageCode)
-                .format(_operation.date),
-            style: Theme.of(context).textTheme.caption,
-          )
-        ],
-      ),
-      leading: CircleAvatar(
-        backgroundColor: Colors.transparent,
-        child: Icon(
-          getOperationIcon(_operation.type),
-          color: getOperationColor(_operation.type),
-        ),
+      trailing: Text(
+        NumberFormat().format(_operation.sum),
+        style: Theme.of(context)
+            .textTheme
+            .headline5
+            .copyWith(color: getOperationColor(_operation.type)),
       ),
       onTap: onTap,
-      onLongPress: () {
-        showModalBottomSheet(
-          context: context,
-          isScrollControlled: true,
-          builder: (context) => Wrap(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.delete),
-                title: Text('DELETE'),
-                onTap: () {
-                  Provider.of<Repository>(context, listen: false)
-                      .deleteOperation(_operation);
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ),
-        );
-      },
+      onLongPress: () => onLongPress(context),
     );
   }
 }
