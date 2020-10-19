f115581aedaa3cdbcca37cffe85a6d86c3c2cd06
pavelegorenkov <epipasha@gmail.com>
2020-10-19 19:27:07 +0300

    Add category table

--- lib/widgets/pages/reports_page.dart
+++ lib/widgets/pages/reports_page.dart
@@ -1,57 +1,82 @@
+import 'package:cashflow/data/objects/category_cashflow_budget.dart';
+import 'package:cashflow/data/operation_type.dart';
+import 'package:cashflow/data/repository.dart';
 import 'package:cashflow/utils/app_localization.dart';
 import 'package:cashflow/widgets/charts/balance_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ReportsPage extends StatelessWidget{
-
+class ReportsPage extends StatelessWidget {
   static const routeName = '/reports';
 
   static open(BuildContext context) {
-    Navigator.of(context)
-        .pushNamed(ReportsPage.routeName);
+    Navigator.of(context).pushNamed(ReportsPage.routeName);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context).titleReports),),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Container(
-              height: 200.0,
-              alignment: Alignment.center,
-              //color: Colors.grey,
-              child: BalanceChart(),
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text(AppLocalizations
+            .of(context)
+            .titleReports),
+      ),
+      body: StreamBuilder<List<CategoryCashflowBudget>>(
+        stream: Provider.of<Repository>(context)
+            .watchAllCategoryCashflowBudget(DateTime.now()),
+        builder: (BuildContext context, AsyncSnapshot<List<CategoryCashflowBudget>> snapshot) {
+          return SingleChildScrollView(child: CategoryTable(categories: snapshot.data));
+        },
       ),
     );
   }
-
 }
 
-//await for (List<CategoryCashflowBudget> categories
-//in _repository.watchAllCategoryCashflowBudget(DateTime.now())) {
-//
-//if(categories.isEmpty){
-//yield Empty();
-//}else {
-//List<CategoryCashflowBudget> categoriesInput = categories
-//    .where((category) => category.type == OperationType.INPUT)
-//    .toList();
-//
-//List<CategoryCashflowBudget> categoriesOutput = categories
-//    .where((category) => category.type == OperationType.OUTPUT)
-//    .toList();
-//
-//yield Success(categoriesInput, categoriesOutput);
-//}
-//}
+class CategoryTable extends StatelessWidget{
 
-//SizedBox(
-//child: CashflowCategoryChart(categories: categories),
-//height: 400.0,
-//)
+  final List<CategoryCashflowBudget> categories;
+
+  const CategoryTable({Key key, this.categories}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+         DataColumn(
+          label: Text(
+            'Category',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          numeric: true,
+          label: Text(
+            'Budget',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          numeric: true,
+          label: Text(
+            'Cashflow',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ],
+      rows: categories
+          .where((c) => c.budget > 0 || c.cashflow > 0)
+          .map((c) =>
+        DataRow(cells: [
+          DataCell(Row(
+            children: [
+              Icon(getOperationIcon(c.type), color: getOperationColor(c.type),),
+              Expanded(child: Text(c.title)),
+            ],
+          )),
+          DataCell(Text('${c.budget}')),
+          DataCell(Text('${c.cashflow}')),
+        ])
+      ).toList(),
+     );
+  }
+}
