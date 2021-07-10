ac1740327c9657205b874e522494763464404400
pavelegorenkov <epipasha@gmail.com>
2021-07-10 14:34:52 +0300

    fix category edit page

--- lib/ui/pages/category/edit_page/category_edit_page_bloc.dart
+++ lib/ui/pages/category/edit_page/category_edit_page_bloc.dart
@@ -2,35 +2,46 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:money_tracker/data/repository.dart';
 import 'package:money_tracker/domain/models.dart';
 
-class CategoryState {
-  final Category? category;
-  final bool editTitleMode;
+abstract class CategoryState{}
 
-  CategoryState(this.editTitleMode, this.category);
+class DateState extends CategoryState{
+  final String title;
+  final int budget;
+  final BudgetType budgetType;
+
+  DateState({required this.budgetType, required this.title, required this.budget});
 }
+
+class Close extends CategoryState{}
 
 class CategoryBloc extends Cubit<CategoryState> {
   final Repository _repository;
 
   late Category _category;
-  bool _editTitleMode = false;
 
-  CategoryBloc(this._repository) : super(CategoryState(false, null));
+  String _title = '';
+  int _budget = 0;
+  BudgetType _budgetType = BudgetType.MONTH;
+
+  CategoryBloc(this._repository) : super(DateState(title: '', budget: 0, budgetType: BudgetType.MONTH));
 
   Future<void> fetch(int id) async {
     _category = await _repository.getCategoryById(id);
-    emit(CategoryState(_editTitleMode, _category));
+
+    _title = _category.title;
+    _budget = _category.budget;
+    _budgetType = _category.budgetType;
+
+    emit(DateState(title: _title, budget: _budget, budgetType: _budgetType));
   }
 
-  void editTitle() {
-    _editTitleMode = true;
-    emit(CategoryState(_editTitleMode, _category));
+  void onBudgetTypeChanged(BudgetType type){
+    _budgetType = type;
   }
 
-  Future<void> saveTitle(String title) async {
-    _editTitleMode = false;
-    _category = _category.copyWith(title: title);
+  Future<void> save(String title, int budget) async {
+    _category = _category.copyWith(title: title, budget: budget, budgetType: _budgetType);
     await _repository.updateCategory(_category);
-    emit(CategoryState(_editTitleMode, _category));
+    emit(Close());
   }
 }
