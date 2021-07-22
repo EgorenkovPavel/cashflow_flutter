deb50424373cd172922ab0a49dd722f9b4504f15
pavelegorenkov <epipasha@gmail.com>
2021-07-22 21:12:08 +0300

    fix

--- lib/domain/models/budget_type.dart
+++ lib/domain/models/budget_type.dart
@@ -1,1 +1,10 @@
 enum BudgetType {MONTH, YEAR}
+
+String getBudgetTypeTitle(BudgetType type){
+  switch(type){
+    case BudgetType.MONTH:
+      return 'Month';
+    case BudgetType.YEAR:
+      return 'Year';
+  }
+}
