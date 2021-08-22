67cb633d90433cf1b09a7e3bf5d21e66b1c99284
pavelegorenkov <epipasha@gmail.com>
2021-08-22 10:51:02 +0300

    fix

---
+++ lib/ui/pages/category/edit_page/category_detail_page_bloc.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:money_tracker/data/repository.dart';
+import 'package:money_tracker/domain/models.dart';
+import 'package:money_tracker/domain/models/sum_on_date.dart';
+
+class CategoryState {
+  final String title;
+  final int budget;
+  final BudgetType budgetType;
+  final List<SumOnDate> cashflows;
+
+  CategoryState(
+      {this.cashflows = const [],
+      this.budgetType = BudgetType.MONTH,
+      this.title = '',
+      this.budget = 0});
+
+  CategoryState copyWith(
+      {List<SumOnDate>? cashflows,
+      BudgetType? budgetType,
+      String? title,
+      int? budget}) {
+    return CategoryState(
+      title: title ?? this.title,
+      budget: budget ?? this.budget,
+      budgetType: budgetType ?? this.budgetType,
+      cashflows: cashflows ?? this.cashflows,
+    );
+  }
+}
+
+class CategoryBloc extends Cubit<CategoryState> {
+  final Repository _repository;
+
+  StreamSubscription<Category>? subCategory;
+
+  CategoryBloc(this._repository) : super(CategoryState());
+
+  Future<void> fetch(int id) async {
+    subCategory = await _repository.watchCategoryById(id).listen((event) {
+      emit(state.copyWith(
+          title: event.title,
+          budget: event.budget,
+          budgetType: event.budgetType));
+    });
+  }
+
+  @override
+  Future<void> close() async {
+    await subCategory?.cancel();
+    return super.close();
+  }
+}
