a000f8c6cec17ce6b0ffcf9611671db17eac2fd0
pavelegorenkov <epipasha@gmail.com>
2021-08-30 20:03:46 +0300

    New version

---
+++ lib/data/database/operation_dao.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'operation_dao.dart';
+
+// **************************************************************************
+// DaoGenerator
+// **************************************************************************
+
+mixin _$OperationDaoMixin on DatabaseAccessor<Database> {
+  $AccountsTable get accounts => attachedDatabase.accounts;
+  $CategoriesTable get categories => attachedDatabase.categories;
+  $OperationsTable get operations => attachedDatabase.operations;
+  $BalancesTable get balances => attachedDatabase.balances;
+  $CashflowsTable get cashflows => attachedDatabase.cashflows;
+}
