02443720d61bca8a190562f60128c95d36230cad
pavelegorenkov <epipasha@gmail.com>
2021-09-05 18:51:25 +0300

    refactoring

---
+++ lib/data/cloud/cloud_account.dart
@@ -0,0 +1,35 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:money_tracker/data/cloud/cloud_converter.dart';
+
+class CloudAccount{
+
+  final String id;
+  final String title;
+
+  CloudAccount({required this.id, required this.title});
+
+}
+
+class CloudAccountConverter extends CloudConverter<CloudAccount>{
+
+  static const String _KEY_TITLE = 'title';
+
+  const CloudAccountConverter();
+
+  @override
+  Map<String, dynamic> mapToCloud(CloudAccount value) {
+    return {
+      _KEY_TITLE: value.title,
+    };
+  }
+
+  @override
+  CloudAccount mapToDart(QueryDocumentSnapshot<Object?> doc) {
+    return CloudAccount(
+      id: doc.id,
+      title: doc.get(_KEY_TITLE),
+    );
+  }
+
+}
---
+++ lib/data/cloud/cloud_operation.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:money_tracker/data/cloud/cloud_converter.dart';
+import 'package:money_tracker/data/database/operation_type_converter.dart';
+import 'package:money_tracker/domain/models/operation_type.dart';
+
+class CloudOperation {
+  final String id;
+  final DateTime date;
+  final OperationType operationType;
+  final String account;
+  final String? category;
+  final String? recAccount;
+  final int sum;
+
+  CloudOperation(
+      {required this.id,
+      required this.date,
+      required this.operationType,
+      required this.account,
+      this.category,
+      this.recAccount,
+      required this.sum});
+}
+
+class CloudOperationConverter extends CloudConverter<CloudOperation> {
+  static const String _KEY_DATE = 'date';
+  static const String _KEY_OPERATION_TYPE = 'operation_type';
+  static const String _KEY_ACCOUNT = 'account';
+  static const String _KEY_CATEGORY = 'category';
+  static const String _KEY_REC_ACCOUNT = 'rec_account';
+  static const String _KEY_SUM = 'sum';
+
+  const CloudOperationConverter();
+
+  @override
+  Map<String, dynamic> mapToCloud(CloudOperation operation) {
+    return {
+      _KEY_DATE: operation.date,
+      _KEY_OPERATION_TYPE:
+          const OperationTypeConverter().mapToSql(operation.operationType),
+      _KEY_ACCOUNT: operation.account,
+      _KEY_CATEGORY: operation.category ?? '',
+      _KEY_REC_ACCOUNT: operation.recAccount ?? '',
+      _KEY_SUM: operation.sum,
+    };
+  }
+
+  @override
+  CloudOperation mapToDart(QueryDocumentSnapshot<Object?> doc) {
+    return CloudOperation(
+      id: doc.id,
+      date: doc.get(_KEY_DATE),
+      operationType: const OperationTypeConverter()
+          .mapToDart(doc.get(_KEY_OPERATION_TYPE))!,
+      account: doc.get(_KEY_ACCOUNT),
+      category: doc.get(_KEY_CATEGORY) == '' ? null : doc.get(_KEY_CATEGORY),
+      recAccount: doc.get(_KEY_REC_ACCOUNT) == '' ? null : doc.get(_KEY_REC_ACCOUNT),
+      sum: doc.get(_KEY_SUM),
+    );
+  }
+}
