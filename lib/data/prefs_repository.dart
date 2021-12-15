1fa7015c4f0877b3d71c86868b40ef9c43de5b7d
pavelegorenkov <epipasha@gmail.com>
2021-12-15 19:06:24 +0300

    Save file name when saving file to google drive

--- lib/data/prefs_repository.dart
+++ lib/data/prefs_repository.dart
@@ -2,6 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class PrefsRepository {
   static const String _KEY_SYNC_DATE = 'sync_date';
+  static const String _KEY_GOOGLE_DRIVE_FILE_NAME = 'google_drive_file_name';
+  static const String _DEFAULT_GOOGLE_DRIVE_FILE_NAME = 'Cashflow backup';
 
   final SharedPreferences prefs;
 
@@ -12,4 +14,11 @@ class PrefsRepository {
 
   Future<void> setSyncDate(DateTime date) async =>
       await prefs.setInt(_KEY_SYNC_DATE, date.millisecondsSinceEpoch);
+
+  String get googleDriveFileName =>
+      prefs.getString(_KEY_GOOGLE_DRIVE_FILE_NAME) ??
+      _DEFAULT_GOOGLE_DRIVE_FILE_NAME;
+
+  Future<void> setGoogleDriveFileName(String name) =>
+      prefs.setString(_KEY_GOOGLE_DRIVE_FILE_NAME, name);
 }
