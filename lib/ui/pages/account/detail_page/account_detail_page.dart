79e8a32138f03c45ca550a834e1c26747c941250
pavelegorenkov <epipasha@gmail.com>
2021-12-21 20:04:43 +0300

    refactoring

--- lib/ui/pages/account/detail_page/account_detail_page.dart
+++ lib/ui/pages/account/detail_page/account_detail_page.dart
@@ -6,52 +6,36 @@ import 'package:money_tracker/ui/page_navigator.dart';
 import 'package:money_tracker/ui/pages/account/detail_page/account_detail_page_bloc.dart';
 import 'package:money_tracker/ui/pages/operation/operation_list.dart';
 
-class AccountDetailPage extends StatefulWidget {
+class AccountDetailPage extends StatelessWidget {
   final int id;
 
   const AccountDetailPage({Key? key, required this.id}) : super(key: key);
 
   @override
-  _AccountDetailPageState createState() => _AccountDetailPageState();
-}
-
-class _AccountDetailPageState extends State<AccountDetailPage> {
-  late AccountDetailPageBloc _bloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _bloc = AccountDetailPageBloc(context.read<LocalSource>());
-    _bloc.fetch(widget.id);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _bloc.close();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: BlocBuilder<AccountDetailPageBloc, AccountEditPageState>(
-          bloc: _bloc,
-          builder: (context, state) => Text(state.accountTitle),
+    return BlocProvider(
+        create: (context) =>
+        AccountDetailPageBloc(context.read<LocalSource>())
+          ..fetch(id),
+        child: Scaffold(
+          appBar: AppBar(
+            title: BlocBuilder<AccountDetailPageBloc, AccountEditPageState>(
+              builder: (context, state) => Text(state.accountTitle),
+            ),
+            actions: <Widget>[
+              IconButton(
+                  onPressed: () =>
+                      PageNavigator.openAccountEditPage(context, id),
+                  icon: Icon(Icons.edit))
+            ],
+          ),
+          body:
+          OperationList(filter: OperationListFilter(accountsIds: {id})),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => PageNavigator.openOperationInputPage(context),
+            child: Icon(Icons.add),
+          ),
         ),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () =>
-                  PageNavigator.openAccountEditPage(context, widget.id),
-              icon: Icon(Icons.edit))
-        ],
-      ),
-      body:
-          OperationList(filter: OperationListFilter(accountsIds: {widget.id})),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => PageNavigator.openOperationInputPage(context),
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
