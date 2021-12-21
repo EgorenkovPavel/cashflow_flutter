875a4538498abdc088871303cca7a3035e41da4d
pavelegorenkov <epipasha@gmail.com>
2021-12-21 20:33:53 +0300

    refactoring

--- lib/ui/pages/category/input_page/category_input_page.dart
+++ lib/ui/pages/category/input_page/category_input_page.dart
@@ -7,7 +7,7 @@ import 'package:money_tracker/ui/pages/item_card.dart';
 import 'package:money_tracker/ui/widgets/type_radio_button.dart';
 import 'package:money_tracker/utils/app_localization.dart';
 
-class CategoryInputPage extends StatefulWidget {
+class CategoryInputPage extends StatelessWidget {
   final OperationType? type;
   final int? id;
 
@@ -16,61 +16,65 @@ class CategoryInputPage extends StatefulWidget {
   const CategoryInputPage.edit({required this.id}) : type = null;
 
   @override
-  _CategoryInputPageState createState() => _CategoryInputPageState();
+  Widget build(BuildContext context) {
+    final _bloc = CategoryInputPageBloc(context.read<LocalSource>());
+    if (id != null) {
+      _bloc.initialById(id!);
+    } else {
+      _bloc.initialByType(type!);
+    }
+    return BlocProvider(
+      create: (context) => _bloc,
+      child: CategoryPage(
+        isNew: id == null,
+      ),
+    );
+  }
 }
 
-class _CategoryInputPageState extends State<CategoryInputPage> {
-  late CategoryInputPageBloc _bloc;
-  final TextEditingController titleController = TextEditingController();
-  final TextEditingController budgetController = TextEditingController();
+class CategoryPage extends StatefulWidget {
+  final bool isNew;
+
+  const CategoryPage({Key? key, required this.isNew}) : super(key: key);
 
   @override
-  void initState() {
-    super.initState();
-    _bloc = CategoryInputPageBloc(context.read<LocalSource>());
-    if (widget.id != null) {
-      _bloc.initialById(widget.id!);
-    } else {
-      _bloc.initialByType(widget.type!);
-    }
-  }
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController budgetController =
+      TextEditingController(text: '0');
 
   @override
   void dispose() {
-    super.dispose();
-    _bloc.close();
     titleController.dispose();
     budgetController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<CategoryInputPageBloc, CategoryInputPageState>(
-      bloc: _bloc,
       listener: (context, state) {
         if (state is Saved) {
           Navigator.of(context).pop(state.category);
+        } else if (state is InitState) {
+          titleController.text = state.title;
+          budgetController.text = state.budget.toString();
         }
       },
+      buildWhen: (previous, current) => current is InputState,
       builder: (context, state) {
         if (!(state is InputState)) {
           return SizedBox();
         }
-
-        if (state.title != null) {
-          titleController.text = state.title!;
-        }
-
-        if (state.budget != null) {
-          budgetController.text = state.budget.toString();
-        }
-
         return ItemCard(
-          title: widget.id == null
+          title: widget.isNew
               ? AppLocalizations.of(context).newCategoryCardTitle
               : AppLocalizations.of(context).categoryCardTitle,
           onSave: (context) {
-            _bloc.save(titleController.text, budgetController.text);
+            context.read<CategoryInputPageBloc>().save();
           },
           child: Column(
             mainAxisSize: MainAxisSize.min,
@@ -87,32 +91,21 @@ class _CategoryInputPageState extends State<CategoryInputPage> {
                     SizedBox(
                       width: 8.0,
                     ),
-                    BlocBuilder<CategoryInputPageBloc, CategoryInputPageState>(
-                        bloc: _bloc,
-                        builder: (context, state) {
-                          if (state is InputState) {
-                            return Text(AppLocalizations.of(context)
-                                .operationTypeTitle(state.type));
-                          } else {
-                            return SizedBox();
-                          }
-                        }),
+                    Text(AppLocalizations.of(context)
+                        .operationTypeTitle(state.type)),
                   ],
                 ),
               ),
-              SizedBox(
-                height: 8.0,
-              ),
+              SizedBox(height: 8.0),
               TextFormField(
                 autofocus: true,
                 controller: titleController,
                 textCapitalization: TextCapitalization.sentences,
                 decoration: InputDecoration(
-                  // focusedBorder: OutlineInputBorder(
-                  //   borderSide: BorderSide(),
-                  // ),
                   labelText: AppLocalizations.of(context).title,
                 ),
+                onChanged: (value) =>
+                    context.read<CategoryInputPageBloc>().changeTitle(value),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return AppLocalizations.of(context).emptyTitleError;
@@ -120,18 +113,15 @@ class _CategoryInputPageState extends State<CategoryInputPage> {
                   return null;
                 },
               ),
-              SizedBox(
-                height: 8.0,
-              ),
+              SizedBox(height: 8.0),
               TextFormField(
                 controller: budgetController,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  // focusedBorder: OutlineInputBorder(
-                  //   borderSide: BorderSide(),
-                  // ),
                   labelText: AppLocalizations.of(context).budget,
                 ),
+                onChanged: (value) =>
+                    context.read<CategoryInputPageBloc>().changeBudget(value),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return AppLocalizations.of(context).emptyTitleError;
@@ -139,19 +129,18 @@ class _CategoryInputPageState extends State<CategoryInputPage> {
                   return null;
                 },
               ),
-              SizedBox(
-                height: 8.0,
-              ),
+              SizedBox(height: 8.0),
               Row(
                 children: [
                   Text(AppLocalizations.of(context).budgetType),
-                  SizedBox(
-                    width: 16.0,
-                  ),
+                  SizedBox(width: 16.0),
                   TypeRadioButton<BudgetType>(
-                      onChange: (type) => _bloc.setBudgetType(type),
-                      type: (state).budgetType,
-                      items: [BudgetType.MONTH, BudgetType.YEAR])
+                    onChange: (type) => context
+                        .read<CategoryInputPageBloc>()
+                        .setBudgetType(type),
+                    type: (state).budgetType,
+                    items: [BudgetType.MONTH, BudgetType.YEAR],
+                  )
                 ],
               ),
             ],
