874f45b9f6fad5ea6fb05e38bd02a0a4d1b58a91
pavelegorenkov <epipasha@gmail.com>
2021-12-21 19:55:41 +0300

    fix

--- lib/ui/pages/account/input_page/account_input_page.dart
+++ lib/ui/pages/account/input_page/account_input_page.dart
@@ -6,41 +6,49 @@ import 'package:money_tracker/ui/pages/account/input_page/account_input_page_blo
 import 'package:money_tracker/ui/pages/item_card.dart';
 import 'package:money_tracker/utils/app_localization.dart';
 
-class AccountInputPage extends StatefulWidget {
-  @override
-  _AccountInputPageState createState() => _AccountInputPageState();
-
+class AccountInputPage extends StatelessWidget {
   final int? id;
 
   const AccountInputPage.input() : id = null;
 
   const AccountInputPage.edit(this.id);
-}
-
-class _AccountInputPageState extends State<AccountInputPage> {
-  final TextEditingController _controller = TextEditingController();
-  late AccountInputPageBloc _bloc;
 
   @override
-  void initState() {
-    super.initState();
-    _bloc = AccountInputPageBloc(context.read<LocalSource>());
-    if (widget.id != null) {
-      _bloc.fetch(widget.id!);
+  Widget build(BuildContext context) {
+    final _bloc = AccountInputPageBloc(context.read<LocalSource>());
+    if (id != null) {
+      _bloc.fetch(id!);
     }
+    return BlocProvider(
+      create: (context) => _bloc,
+      child: AccountPage(
+        isNew: id == null,
+      ),
+    );
   }
+}
+
+class AccountPage extends StatefulWidget {
+  final bool isNew;
+
+  const AccountPage({Key? key, required this.isNew}) : super(key: key);
+
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  final TextEditingController _controller = TextEditingController();
 
   @override
   void dispose() {
-    super.dispose();
     _controller.dispose();
-    _bloc.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AccountInputPageBloc, AccountInputPageState>(
-      bloc: _bloc,
       listener: (context, state) {
         if (state is Close) {
           Navigator.of(context).pop(state.account);
@@ -51,10 +59,10 @@ class _AccountInputPageState extends State<AccountInputPage> {
       buildWhen: (previous, current) => current is Data,
       builder: (context, state) {
         return ItemCard<Account>(
-          title: widget.id == null
+          title: widget.isNew
               ? AppLocalizations.of(context).newAccountCardTitle
               : AppLocalizations.of(context).accountCardTitle,
-          onSave: (context) => _bloc.save(),
+          onSave: (context) => context.read<AccountInputPageBloc>().save(),
           child: Column(
             children: [
               TextFormField(
@@ -64,7 +72,8 @@ class _AccountInputPageState extends State<AccountInputPage> {
                 decoration: InputDecoration(
                   labelText: AppLocalizations.of(context).title,
                 ),
-                onChanged: (value) => _bloc.changeTitle(value),
+                onChanged: (value) =>
+                    context.read<AccountInputPageBloc>().changeTitle(value),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return AppLocalizations.of(context).emptyTitleError;
@@ -76,7 +85,8 @@ class _AccountInputPageState extends State<AccountInputPage> {
                 children: [
                   Checkbox(
                     value: (state as Data).isDebt,
-                    onChanged: (val) => _bloc.changeIsDebt(val!),
+                    onChanged: (val) =>
+                        context.read<AccountInputPageBloc>().changeIsDebt(val!),
                   ),
                   Text('Is debt'),
                 ],
