07cf5ac732185ef39236749cadbb08076d9697e3
pavelegorenkov <epipasha@gmail.com>
2021-12-21 20:43:09 +0300

    refactoring

--- lib/ui/pages/category/detail_page/category_detail_page.dart
+++ lib/ui/pages/category/detail_page/category_detail_page.dart
@@ -9,76 +9,56 @@ import 'package:money_tracker/ui/pages/operation/list_divider_operation.dart';
 import 'package:money_tracker/ui/pages/operation/list_tile_operation.dart';
 import 'package:money_tracker/utils/app_localization.dart';
 
-class CategoryDetailPage extends StatefulWidget {
+class CategoryDetailPage extends StatelessWidget {
   final int id;
 
   const CategoryDetailPage({Key? key, required this.id}) : super(key: key);
 
   @override
-  _CategoryDetailPageState createState() => _CategoryDetailPageState();
-}
-
-class _CategoryDetailPageState extends State<CategoryDetailPage>
-    with SingleTickerProviderStateMixin {
-  late CategoryBloc _bloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _bloc = CategoryBloc(context.read<LocalSource>())..fetch(widget.id);
-  }
-
-  @override
-  void dispose() {
-    _bloc.close();
-    super.dispose();
-  }
-
-  //TODO add sliver list
-
-  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CategoryBloc, CategoryState>(
-      bloc: _bloc,
-      builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(state.title),
-            actions: [
-              IconButton(
-                  onPressed: () => PageNavigator.openCategoryEditPage(context,
-                      id: widget.id),
-                  icon: Icon(Icons.edit))
-            ],
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                      '${AppLocalizations.of(context).budget} ${state.budget} ${AppLocalizations.of(context).in_period} ${AppLocalizations.of(context).budgetTypeTitle(state.budgetType)}'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                      height: 200.0,
-                      child: CategoryCashflowDiagram(
-                        id: widget.id,
-                        budget: state.budget,
-                        budgetType: state.budgetType,
-                      )),
-                ),
-                _OperationList(id: widget.id),
+    return BlocProvider(
+      create: (context) => CategoryBloc(context.read<LocalSource>())..fetch(id),
+      child: BlocBuilder<CategoryBloc, CategoryState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(state.title),
+              actions: [
+                IconButton(
+                    onPressed: () =>
+                        PageNavigator.openCategoryEditPage(context, id: id),
+                    icon: Icon(Icons.edit))
               ],
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => PageNavigator.openOperationInputPage(context),
-            child: Icon(Icons.add),
-          ),
-        );
-      },
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                        '${AppLocalizations.of(context).budget} ${state.budget} ${AppLocalizations.of(context).in_period} ${AppLocalizations.of(context).budgetTypeTitle(state.budgetType)}'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                        height: 200.0,
+                        child: CategoryCashflowDiagram(
+                          id: id,
+                          budget: state.budget,
+                          budgetType: state.budgetType,
+                        )),
+                  ),
+                  _OperationList(id: id),
+                ],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => PageNavigator.openOperationInputPage(context),
+              child: Icon(Icons.add),
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -106,9 +86,11 @@ class _OperationList extends StatelessWidget {
                     ListDividerOperation.day(null, e)
                   else
                     ListDividerOperation.day(list[list.indexOf(e) - 1], e),
-                  ListTileOperation(e,
-                      onTap: () =>
-                          PageNavigator.openOperationEditPage(context, e.id))
+                  ListTileOperation(
+                    e,
+                    onTap: () =>
+                        PageNavigator.openOperationEditPage(context, e.id),
+                  )
                 ],
               )
               .toList(),
