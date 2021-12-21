f37ed090507a76bef0022e4eb9426607c99ed483
pavelegorenkov <epipasha@gmail.com>
2021-12-21 20:33:53 +0300

    refactoring

--- lib/ui/pages/category/input_page/category_input_page_bloc.dart
+++ lib/ui/pages/category/input_page/category_input_page_bloc.dart
@@ -4,17 +4,20 @@ import 'package:money_tracker/domain/models.dart';
 
 abstract class CategoryInputPageState {}
 
+class InitState extends CategoryInputPageState {
+  final String title;
+  final int budget;
+
+  InitState({required this.title, required this.budget});
+}
+
 class InputState extends CategoryInputPageState {
   final OperationType type;
   final BudgetType budgetType;
-  final String? title;
-  final int? budget;
 
   InputState({
     required this.type,
     required this.budgetType,
-    this.title,
-    this.budget,
   });
 }
 
@@ -30,50 +33,74 @@ class CategoryInputPageBloc extends Cubit<CategoryInputPageState> {
   Category? _category;
   OperationType _type = OperationType.INPUT;
   BudgetType _budgetType = BudgetType.MONTH;
+  String _title = '';
+  int _budget = 0;
 
   CategoryInputPageBloc(this._repository)
       : super(InputState(
-            type: OperationType.INPUT, budgetType: BudgetType.MONTH));
+          type: OperationType.INPUT,
+          budgetType: BudgetType.MONTH,
+        ));
 
   void initialByType(OperationType type) {
     _type = type;
-    emit(
-        InputState(type: _type, budgetType: _budgetType, title: '', budget: 0));
+    emit(InputState(
+      type: _type,
+      budgetType: _budgetType,
+    ));
   }
 
   Future<void> initialById(int id) async {
     _category = await _repository.categories.getById(id);
     _type = _category!.operationType;
     _budgetType = _category!.budgetType;
+    _title = _category!.title;
+    _budget = _category!.budget;
+
+    emit(InitState(
+      title: _title,
+      budget: _budget,
+    ));
+
     emit(InputState(
-        type: _type,
-        budgetType: _budgetType,
-        title: _category!.title,
-        budget: _category!.budget));
+      type: _type,
+      budgetType: _budgetType,
+    ));
+  }
+
+  void changeTitle(String title){
+    _title = title;
+  }
+
+  void changeBudget(String budget){
+    _budget = int.parse(budget);
   }
 
   void setBudgetType(BudgetType budgetType) {
     _budgetType = budgetType;
-    emit(InputState(type: _type, budgetType: _budgetType));
+    emit(InputState(
+      type: _type,
+      budgetType: _budgetType,
+    ));
   }
 
-  Future<void> save(String title, String budget) async {
+  Future<void> save() async {
     if (_category == null) {
       var category = Category(
-        title: title,
+        title: _title,
         operationType: _type,
         budgetType: _budgetType,
-        budget: int.parse(budget),
+        budget: _budget,
       );
 
       var id = await _repository.categories.insert(category);
       emit(Saved(category.copyWith(id: id)));
     } else {
       var _newCategory = _category!.copyWith(
-        title: title,
+        title: _title,
         operationType: _type,
         budgetType: _budgetType,
-        budget: int.parse(budget),
+        budget: _budget,
       );
 
       await _repository.categories.update(_newCategory);
