43ea8f8e0bee93d713bb5d5b7e9e8f8a2f504fbf
pavelegorenkov <epipasha@gmail.com>
2022-01-19 20:20:24 +0300

    design

--- lib/ui/pages/home/month_operations.dart
+++ lib/ui/pages/home/month_operations.dart
@@ -35,21 +35,18 @@ class MonthOperations extends StatelessWidget {
               ),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                width: Dimensions.padding,
-              ),
-              _MonthOperation(operationType: OperationType.INPUT),
-              SizedBox(
-                width: Dimensions.padding,
-              ),
-              _MonthOperation(operationType: OperationType.OUTPUT),
-              SizedBox(
-                width: Dimensions.padding,
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _MonthOperation(operationType: OperationType.INPUT),
+                SizedBox(
+                  width: Dimensions.padding,
+                ),
+                _MonthOperation(operationType: OperationType.OUTPUT),
+              ],
+            ),
           ),
         ],
       ),
@@ -110,90 +107,147 @@ class _MonthOperation extends StatelessWidget {
           _progress = _cashflow / _budget;
         }
 
-        return Expanded(
-          child: InkWell(
-            onTap: () => PageNavigator.openBudgetPage(context, operationType),
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
+        return InkWell(
+          onTap: () => PageNavigator.openBudgetPage(context, operationType),
+          // child: Card(
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(16.0),
+          //     child: Column(
+          //       children: [
+          //         Text(
+          //             operationType == OperationType.INPUT
+          //                 ? AppLocalizations.of(context).earning
+          //                 : AppLocalizations.of(context).spending,
+          //             style: Theme.of(context).textTheme.headline6),
+          //         SizedBox(
+          //           height: 16,
+          //         ),
+          //         Stack(
+          //           children: [
+          //             AspectRatio(
+          //               aspectRatio: 1,
+          //               child: CircularProgressIndicator(
+          //                 value: _progress,
+          //                 strokeWidth: 10.0,
+          //               ),
+          //             ),
+          //             AspectRatio(
+          //               aspectRatio: 1,
+          //               child: Center(
+          //                 child: Column(
+          //                   mainAxisAlignment: MainAxisAlignment.center,
+          //                   children: [
+          //                     Text(
+          //                       AppLocalizations.of(context)
+          //                           .numberFormat(_cashflow),
+          //                       style: Theme.of(context)
+          //                           .textTheme
+          //                           .headline6!
+          //                           .copyWith(
+          //                             fontSize: 20,
+          //                             fontWeight: FontWeight.bold,
+          //                           ),
+          //                     ),
+          //                     Text(
+          //                       AppLocalizations.of(context)
+          //                           .numberFormat(_budget),
+          //                     ),
+          //                   ],
+          //                 ),
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          child: Container(
+            padding: EdgeInsets.all(Dimensions.padding),
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).colorScheme.primary),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Column(
+              children: [
+                Text(
+                    operationType == OperationType.INPUT
+                        ? AppLocalizations.of(context).earning
+                        : AppLocalizations.of(context).spending,
+                    style: Theme.of(context).textTheme.headline6),
+                Stack(
                   children: [
-                    Text(
-                        operationType == OperationType.INPUT
-                            ? AppLocalizations.of(context).earning
-                            : AppLocalizations.of(context).spending,
-                        style: Theme.of(context).textTheme.headline6),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Stack(
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 1,
-                          child: CircularProgressIndicator(
-                            value: _progress,
-                            strokeWidth: 10.0,
-                          ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).colorScheme.primary),
+                          borderRadius: BorderRadius.circular(2.0),
                         ),
-                        AspectRatio(
-                          aspectRatio: 1,
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  AppLocalizations.of(context)
-                                      .numberFormat(_cashflow),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline6!
-                                      .copyWith(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                                Text(
-                                  AppLocalizations.of(context)
-                                      .numberFormat(_budget),
-                                ),
-                              ],
-                            ),
-                          ),
+                        child: LinearProgressIndicator(
+                          minHeight: 10,
+                          color: Theme.of(context).colorScheme.primary,
+                          value: _progress,
                         ),
-                      ],
+                      ),
                     ),
+                    Align(
+                      alignment: Alignment(getAlign(), 0.0),
+                      child: Container(
+                        color: Colors.black,
+                        width: 2.0,
+                        height: 32.0,
+                      ),
+                    )
                   ],
                 ),
-              ),
+                Text(AppLocalizations.of(context).numberFormat(_cashflow)),
+              ],
             ),
-            // child: Container(
-            //   padding: EdgeInsets.all(Dimensions.padding),
-            //   decoration: BoxDecoration(
-            //     border: Border.all(color: Theme.of(context).colorScheme.primary),
-            //     borderRadius: BorderRadius.circular(8.0),
-            //   ),
-            //   child: Column(
-            //     children: [
-            //       Text(
-            //           operationType == OperationType.INPUT
-            //               ? AppLocalizations.of(context).earning
-            //               : AppLocalizations.of(context).spending,
-            //           style: Theme.of(context).textTheme.headline6),
-            //       Padding(
-            //         padding: const EdgeInsets.symmetric(vertical: 8.0),
-            //         child: LinearProgressIndicator(
-            //           minHeight: 10,
-            //           color: Theme.of(context).colorScheme.secondary,
-            //           value: _progress,
-            //         ),
-            //       ),
-            //       Text(AppLocalizations.of(context).numberFormat(_cashflow)),
-            //     ],
-            //   ),
-            // ),
           ),
         );
       },
     );
+  }
+
+  double getAlign(){
+    var now = DateTime.now();
+    var days = daysInMonth(now.month, now.year);
+    return now.day / days * 2 - 1;
+  }
+
+  int daysInMonth(final int monthNum, final int year) {
+    List<int> monthLength = List.filled(12, 0);
+
+    monthLength[0] = 31;
+    monthLength[2] = 31;
+    monthLength[4] = 31;
+    monthLength[6] = 31;
+    monthLength[7] = 31;
+    monthLength[9] = 31;
+    monthLength[11] = 31;
+    monthLength[3] = 30;
+    monthLength[8] = 30;
+    monthLength[5] = 30;
+    monthLength[10] = 30;
+
+    if (leapYear(year) == true)
+      monthLength[1] = 29;
+    else
+      monthLength[1] = 28;
+
+    return monthLength[monthNum - 1];
+  }
+
+  bool leapYear(int year) {
+    bool leapYear = false;
+
+    bool leap = ((year % 100 == 0) && (year % 400 != 0));
+    if (leap == true)
+      leapYear = false;
+    else if (year % 4 == 0) leapYear = true;
+
+    return leapYear;
   }
 }
