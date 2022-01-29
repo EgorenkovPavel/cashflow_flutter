83ccb933611a27869b94a4c0fba0e211e5fdf611
pavelegorenkov <epipasha@gmail.com>
2022-01-29 21:12:20 +0300

    refactoring operation_edit_page.dart

---
+++ lib/ui/pages/operation/edit_page/operation_edit_bloc.dart
@@ -0,0 +1,315 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:money_tracker/data/local/local_source.dart';
+import 'package:money_tracker/domain/models.dart';
+
+abstract class OperationEditEvent {}
+
+class Fetch extends OperationEditEvent {
+  final int operationId;
+
+  Fetch(this.operationId);
+}
+
+class ChangeDate extends OperationEditEvent {
+  final DateTime date;
+
+  ChangeDate(this.date);
+}
+
+class ChangeTime extends OperationEditEvent {
+  final TimeOfDay time;
+
+  ChangeTime(this.time);
+}
+
+class ChangeOperationType extends OperationEditEvent {
+  final OperationType operationType;
+
+  ChangeOperationType(this.operationType);
+}
+
+class ChangeAccount extends OperationEditEvent {
+  final Account account;
+
+  ChangeAccount(this.account);
+}
+
+class ChangeCategory extends OperationEditEvent {
+  final Category category;
+
+  ChangeCategory(this.category);
+}
+
+class ChangeRecAccount extends OperationEditEvent {
+  final Account recAccount;
+
+  ChangeRecAccount(this.recAccount);
+}
+
+class ChangeSum extends OperationEditEvent {
+  final int sum;
+
+  ChangeSum(this.sum);
+}
+
+class Save extends OperationEditEvent {}
+
+abstract class OperationEditState {
+  final Operation operation;
+  final DateTime date;
+  final TimeOfDay time;
+  final OperationType operationType;
+  final Account account;
+  final Category? category;
+  final Account? recAccount;
+  final int sum;
+
+  final List<Account> accounts;
+  final List<Category> inCategories;
+  final List<Category> outCategories;
+
+  OperationEditState({
+    required this.operation,
+    required this.date,
+    required this.time,
+    required this.operationType,
+    required this.account,
+    this.category,
+    this.recAccount,
+    required this.sum,
+    required this.accounts,
+    required this.inCategories,
+    required this.outCategories,
+  });
+}
+
+class Data extends OperationEditState {
+  Data(
+      Operation operation,
+      DateTime date,
+      TimeOfDay time,
+      OperationType operationType,
+      Account account,
+      Category? category,
+      Account? recAccount,
+      int sum,
+      List<Account> accounts,
+      List<Category> inCategories,
+      List<Category> outCategories)
+      : super(
+          operation: operation,
+          date: date,
+          time: time,
+          operationType: operationType,
+          account: account,
+          category: category,
+          recAccount: recAccount,
+          sum: sum,
+          accounts: accounts,
+          inCategories: inCategories,
+          outCategories: outCategories,
+        );
+
+  Data.initial()
+      : super(
+          operation: Operation(
+              date: DateTime.now(),
+              type: OperationType.INPUT,
+              account: Account(title: '', isDebt: false),
+              sum: 0),
+          date: DateTime.now(),
+          time: TimeOfDay.now(),
+          operationType: OperationType.INPUT,
+          account: Account(title: '', isDebt: false),
+          sum: 0,
+          accounts: [],
+          inCategories: [],
+          outCategories: [],
+        );
+
+  Data.state({
+    required OperationEditState state,
+    Operation? operation,
+    DateTime? date,
+    TimeOfDay? time,
+    OperationType? operationType,
+    Account? account,
+    Category? category,
+    Account? recAccount,
+    int? sum,
+    List<Account>? accounts,
+    List<Category>? inCategories,
+    List<Category>? outCategories,
+  }) : super(
+          operation: operation ?? state.operation,
+          date: date ?? state.date,
+          time: time ?? state.time,
+          operationType: operationType ?? state.operationType,
+          account: account ?? state.account,
+          category: category ?? state.category,
+          recAccount: recAccount ?? state.recAccount,
+          sum: sum ?? state.sum,
+          accounts: accounts ?? state.accounts,
+          inCategories: inCategories ?? state.inCategories,
+          outCategories: outCategories ?? state.outCategories,
+        );
+
+  Data.resetCategory({
+    required OperationEditState state,
+  }) : super(
+          operation: state.operation,
+          date: state.date,
+          time: state.time,
+          operationType: state.operationType,
+          account: state.account,
+          category: null,
+          recAccount: state.recAccount,
+          sum: state.sum,
+          accounts: state.accounts,
+          inCategories: state.inCategories,
+          outCategories: state.outCategories,
+        );
+}
+
+class FetchOperation extends OperationEditState {
+  FetchOperation(
+      {required Operation operation, required OperationEditState state})
+      : super(
+          operation: operation,
+          operationType: operation.type,
+          date: operation.date,
+          account: operation.account,
+          category: operation.category,
+          recAccount: operation.recAccount,
+          sum: operation.sum,
+          time: TimeOfDay.fromDateTime(operation.date),
+          accounts: state.accounts,
+          inCategories: state.inCategories,
+          outCategories: state.outCategories,
+        );
+}
+
+class Saved extends OperationEditState {
+  Saved({required OperationEditState state})
+      : super(
+          operation: state.operation,
+          date: state.date,
+          time: state.time,
+          operationType: state.operationType,
+          account: state.account,
+          category: state.category,
+          recAccount: state.recAccount,
+          sum: state.sum,
+          accounts: state.accounts,
+          inCategories: state.inCategories,
+          outCategories: state.outCategories,
+        );
+}
+
+class OperationEditBloc extends Bloc<OperationEditEvent, OperationEditState> {
+  final LocalSource _repository;
+
+  OperationEditBloc(this._repository) : super(Data.initial()) {
+    on<Fetch>(_fetch);
+    on<ChangeDate>(_changeDate);
+    on<ChangeTime>(_changeTime);
+    on<ChangeOperationType>(_changeOperationType);
+    on<ChangeAccount>(_changeAccount);
+    on<ChangeCategory>(_changeCategory);
+    on<ChangeRecAccount>(_changeRecAccount);
+    on<ChangeSum>(_changeSum);
+    on<Save>(_save);
+  }
+
+  Future<void> _fetch(Fetch event, Emitter<OperationEditState> emit) async {
+    final _operation = await _repository.operations.getById(event.operationId);
+    emit(FetchOperation(operation: _operation, state: state));
+
+    final _accounts = await _repository.accounts.getAll();
+    final _categories = await _repository.categories.getAll();
+
+    emit(Data.state(
+      state: state,
+      accounts: _accounts,
+      inCategories: _categories
+          .where((category) => category.operationType == OperationType.INPUT)
+          .toList(),
+      outCategories: _categories
+          .where((category) => category.operationType == OperationType.OUTPUT)
+          .toList(),
+    ));
+  }
+
+  void _changeDate(ChangeDate event, Emitter<OperationEditState> emit) {
+    emit(Data.state(state: state, date: event.date));
+  }
+
+  void _changeTime(ChangeTime event, Emitter<OperationEditState> emit) {
+    emit(Data.state(state: state, time: event.time));
+  }
+
+  void _changeOperationType(
+      ChangeOperationType event, Emitter<OperationEditState> emit) {
+    emit(Data.state(state: state, operationType: event.operationType));
+    emit(Data.resetCategory(state: state));
+  }
+
+  void _changeAccount(ChangeAccount event, Emitter<OperationEditState> emit) {
+    emit(Data.state(state: state, account: event.account));
+  }
+
+  void _changeCategory(ChangeCategory event, Emitter<OperationEditState> emit) {
+    emit(Data.state(state: state, category: event.category));
+  }
+
+  void _changeRecAccount(
+      ChangeRecAccount event, Emitter<OperationEditState> emit) {
+    emit(Data.state(state: state, recAccount: event.recAccount));
+  }
+
+  void _changeSum(ChangeSum event, Emitter<OperationEditState> emit) {
+    emit(Data.state(state: state, sum: event.sum));
+  }
+
+  Future<void> _save(Save event, Emitter<OperationEditState> emit) async {
+    final _date = DateTime(
+      state.date.year,
+      state.date.month,
+      state.date.day,
+      state.time.hour,
+      state.time.minute,
+    );
+    if (state.operationType == OperationType.TRANSFER) {
+      var _newOperation = state.operation.copyWith(
+        date: _date,
+        type: state.operationType,
+        account: state.account,
+        category: null,
+        recAccount: state.recAccount,
+        sum: state.sum,
+      );
+
+      await _repository
+          .operations
+          .insert(_newOperation);
+    } else {
+      var _newOperation = state.operation.copyWith(
+        date: _date,
+        type: state.operationType,
+        account: state.account,
+        category: state.category,
+        recAccount: null,
+        sum: state.sum,
+      );
+
+      await _repository
+          .operations
+          .update(_newOperation);
+    }
+
+    emit(Saved(state: state));
+  }
+}
---
+++ lib/ui/pages/operation/edit_page/operation_edit_page.dart
@@ -0,0 +1,279 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+import 'package:money_tracker/data/local/local_source.dart';
+import 'package:money_tracker/domain/models.dart';
+import 'package:money_tracker/ui/pages/operation/edit_page/operation_edit_bloc.dart';
+import 'package:money_tracker/ui/widgets/dropdown_list.dart';
+import 'package:money_tracker/ui/widgets/type_radio_button.dart';
+import 'package:money_tracker/utils/app_localization.dart';
+
+class OperationEditPage extends StatelessWidget {
+  final int id;
+
+  const OperationEditPage({Key? key, required this.id}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (context) =>
+    OperationEditBloc(context.read<LocalSource>())..add(Fetch(id)),
+    child: _OperationEditPage(),
+    );
+  }
+}
+
+class _OperationEditPage extends StatefulWidget {
+
+  @override
+  _OperationEditPageState createState() => _OperationEditPageState();
+}
+
+class _OperationEditPageState extends State<_OperationEditPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _sumController = TextEditingController();
+
+  @override
+  void dispose() {
+    _sumController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<OperationEditBloc, OperationEditState>(
+      listener: (context, state) {
+        if (state is FetchOperation) {
+          _sumController.text = state.sum.toString();
+        } else if (state is Saved) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Form(
+        key: _formKey,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(AppLocalizations.of(context).operationCardTitle),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Title(text: AppLocalizations.of(context).titleDate),
+                  Row(
+                    children: <Widget>[
+                      DateButton(
+                          icon: Icons.calendar_today,
+                          text: DateFormat.yMMMd(
+                              Localizations.localeOf(context)
+                                  .languageCode)
+                              .format(context.select<OperationEditBloc,
+                              DateTime>((bloc) => bloc.state.date)),
+                          onPressed: _selectDate),
+                      SizedBox(width: 16.0),
+                      DateButton(
+                          icon: Icons.access_time,
+                          text: context
+                              .select<OperationEditBloc, TimeOfDay>(
+                                  (bloc) => bloc.state.time)
+                              .format(context),
+                          onPressed: _selectTime),
+                    ],
+                  ),
+                  Title(text: AppLocalizations.of(context).titleType),
+                  TypeRadioButton<OperationType>(
+                    type: context.select<OperationEditBloc, OperationType>(
+                            (bloc) => bloc.state.operationType),
+                    onChange: (newValue) => context
+                        .read<OperationEditBloc>()
+                        .add(ChangeOperationType(newValue)),
+                    items: [
+                      OperationType.INPUT,
+                      OperationType.OUTPUT,
+                      OperationType.TRANSFER
+                    ],
+                  ),
+                  Title(text: AppLocalizations.of(context).titleAccount),
+                  DropdownList<Account>(
+                    value: context.select<OperationEditBloc, Account>(
+                            (bloc) => bloc.state.account),
+                    hint: AppLocalizations.of(context).hintAccount,
+                    items: context.select<OperationEditBloc, List<Account>>(
+                            (bloc) => bloc.state.accounts),
+                    onChange: (newValue) {
+                      if (newValue != null) {
+                        context
+                            .read<OperationEditBloc>()
+                            .add(ChangeAccount(newValue));
+                      }
+                    },
+                    getListItem: (data) =>
+                        ListTile(title: Text(data.title)),
+                  ),
+                  Title(text: AppLocalizations.of(context).titleAnalytic),
+                  analyticMenu(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: _sumController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: AppLocalizations.of(context).titleSum,
+                      ),
+                      onChanged: (value) => context
+                          .read<OperationEditBloc>()
+                          .add(ChangeSum(int.parse(value))),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return AppLocalizations.of(context).emptySumError;
+                        }
+                        return null;
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          persistentFooterButtons: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child:
+              Text(AppLocalizations.of(context).cancel.toUpperCase()),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  context.read<OperationEditBloc>().add(Save());
+                }
+              },
+              child: Text(AppLocalizations.of(context).save.toUpperCase()),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget analyticMenu() {
+    return BlocBuilder<OperationEditBloc, OperationEditState>(
+        builder: (context, state) {
+      switch (state.operationType) {
+        case OperationType.INPUT:
+          return DropdownList<Category>(
+              value: state.category,
+              hint: AppLocalizations.of(context).hintCategory,
+              onChange: (Category? newValue) {
+                if (newValue != null) {
+                  context
+                      .read<OperationEditBloc>()
+                      .add(ChangeCategory(newValue));
+                }
+              },
+              items: state.inCategories,
+              getListItem: (item) => ListTile(title: Text(item.title)));
+        case OperationType.OUTPUT:
+          return DropdownList<Category>(
+              value: state.category,
+              hint: AppLocalizations.of(context).hintCategory,
+              onChange: (Category? newValue) {
+                if (newValue != null) {
+                  context
+                      .read<OperationEditBloc>()
+                      .add(ChangeCategory(newValue));
+                }
+              },
+              items: state.outCategories,
+              getListItem: (item) => ListTile(title: Text(item.title)));
+        case OperationType.TRANSFER:
+          return DropdownList<Account>(
+              value: state.recAccount,
+              hint: AppLocalizations.of(context).hintAccount,
+              onChange: (Account? newValue) {
+                if (newValue != null) {
+                  context
+                      .read<OperationEditBloc>()
+                      .add(ChangeRecAccount(newValue));
+                }
+              },
+              items: state.accounts,
+              getListItem: (item) => ListTile(title: Text(item.title)));
+        default:
+          return SizedBox();
+      }
+    });
+  }
+
+  void _selectDate() async {
+    final picked = await showDatePicker(
+      context: context,
+      initialDate: context.read<OperationEditBloc>().state.date,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null) {
+      context.read<OperationEditBloc>().add(ChangeDate(picked));
+    }
+  }
+
+  void _selectTime() async {
+    final picked = await showTimePicker(
+        context: context,
+        initialTime: context.read<OperationEditBloc>().state.time);
+    if (picked != null) {
+      context.read<OperationEditBloc>().add(ChangeTime(picked));
+    }
+  }
+}
+
+class DateButton extends StatelessWidget {
+  const DateButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String text;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Row(
+        children: <Widget>[
+          Icon(icon),
+          SizedBox(width: 4.0),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 0.0, top: 8.0),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.caption,
+      ),
+    );
+  }
+}
