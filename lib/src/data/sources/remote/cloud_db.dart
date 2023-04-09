5b76c3cbdebadff54fa006bbdd24af83356f2630
pavelegorenkov <epipasha@gmail.com>
2023-04-09 18:55:08 +0300

    Add cloud DB

--- lib/src/data/sources/remote/cloud_db.dart
+++ lib/src/data/sources/remote/cloud_db.dart
@@ -1,7 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class CloudDb {
+import 'daos/table_dao.dart';
+import 'mappers/account_mapper.dart';
+import 'mappers/category_mapper.dart';
+import 'mappers/operation_mapper.dart';
+import 'mappers/user_mapper.dart';
+import 'package:money_tracker/src/domain/models.dart';
 
+import 'models/cloud_models.dart';
+
+class CloudDb {
   static const String _DATABASES = 'databases';
   static const String _DATABASES_USERS = 'users';
   static const String _DATABASES_ADMIN = 'admin';
@@ -10,23 +18,49 @@ class CloudDb {
   static const String _OPERATIONS = 'operations';
   static const String _USERS = 'users';
 
-  DocumentReference<Map<String, dynamic>?> _db;
+  final DocumentReference<Map<String, dynamic>?> _db;
+  final bool _isCurrentUserAdmin;
 
-  CloudDb(this._db);
+  CloudDb(this._db, this._isCurrentUserAdmin);
 
-  static Future<CloudDb?> findByUserId(FirebaseFirestore firestore, String userId) async {
+  bool get isCurrentUserAdmin => _isCurrentUserAdmin;
+
+  static Future<CloudDb?> findByUserId(
+    FirebaseFirestore firestore,
+    String userId,
+  ) async {
     final db = await _getDatabase(firestore, userId);
-    if (db == null){
+    if (db == null) {
       return null;
-    }else{
-      return CloudDb(db);
-    }
+    } else {
+      var doc = await db.get();
 
+      final isUserAdmin = userId == doc.data()?[_DATABASES_ADMIN];
+
+      return CloudDb(db, isUserAdmin);
+    }
+  }
+
+  static Future<CloudDb> create(
+    FirebaseFirestore firestore,
+    User user,
+  ) async {
+    final db = await firestore.collection(_DATABASES).add({
+      _DATABASES_ADMIN: user.id,
+      _DATABASES_USERS: [user.id],
+    });
+    await db
+        .collection(_USERS)
+        .doc(user.id)
+        .set(const UserMapper().mapToCloud(user));
+
+    return CloudDb(db, true);
   }
 
   static Future<DocumentReference<Map<String, dynamic>?>?> _getDatabase(
-      FirebaseFirestore firestore,
-      String userId,) async {
+    FirebaseFirestore firestore,
+    String userId,
+  ) async {
     QuerySnapshot querySnapshot;
     querySnapshot = await firestore
         .collection(_DATABASES)
@@ -35,8 +69,58 @@ class CloudDb {
 
     if (querySnapshot.docs.isNotEmpty) {
       return firestore.collection(_DATABASES).doc(querySnapshot.docs.first.id);
-    }else{
+    } else {
       return null;
     }
+  }
+
+  static Future<bool> databaseExists(FirebaseFirestore firestore, User user) async {
+    QuerySnapshot querySnapshot = await firestore
+        .collection(_DATABASES)
+        .where(_DATABASES_USERS, arrayContains: user.id)
+        .get();
+
+    return querySnapshot.docs.isNotEmpty;
+  }
+
+  @override
+  TableDAO<CloudAccount>? get accounts =>
+      AccountsDAO(
+    collection: _db.collection(_ACCOUNTS),
+    key_updated: AccountMapper.KEY_UPDATED,
+    mapper: const AccountMapper(),
+  );
+
+  @override
+  TableDAO<CloudCategory>? get categories => CategoriesDAO(
+    collection: _db.collection(_CATEGORIES),
+    key_updated: CategoryMapper.KEY_UPDATED,
+    mapper: const CategoryMapper(),
+  );
+
+  @override
+  TableDAO<CloudOperation>? get operations => OperationDAO(
+    collection: _db.collection(_OPERATIONS),
+    key_updated: OperationMapper.KEY_UPDATED,
+    mapper: const OperationMapper(),
+  );
+
+  Future<void> addUserToDatabase(User user) async {
+    var doc = await _db.get();
+    var data = doc.data();
+    data?[_DATABASES_USERS].add(user.id);
+    await _db.set(data);
+
+    await _db
+        .collection(_USERS)
+        .doc(user.id)
+        .set(const UserMapper().mapToCloud(user));
+  }
+
+  Future<List<User>> getAllUsers() async {
+
+    var doc = await _db.collection(_USERS).get();
+
+    return doc.docs.map((doc) => const UserMapper().mapToDart(doc)).toList();
   }
 }
--- lib/src/data/sources/remote/remote_source_impl.dart
+++ lib/src/data/sources/remote/remote_source_impl.dart
@@ -3,184 +3,63 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'mappers/account_mapper.dart';
-import 'mappers/category_mapper.dart';
-import 'mappers/operation_mapper.dart';
-import 'mappers/user_mapper.dart';
+import 'cloud_db.dart';
 import 'package:money_tracker/src/data/interfaces/remote_data_source.dart';
 import 'package:money_tracker/src/data/sources/remote/daos/table_dao.dart';
 import 'package:money_tracker/src/domain/models/user.dart';
-import 'package:money_tracker/src/utils/exceptions.dart';
 
 import 'models/cloud_models.dart';
 
 class RemoteSourceImpl extends RemoteDataSource {
   final FirebaseFirestore _firestore;
 
-  static const String _DATABASES = 'databases';
-  static const String _DATABASES_USERS = 'users';
-  static const String _DATABASES_ADMIN = 'admin';
-  static const String _ACCOUNTS = 'accounts';
-  static const String _CATEGORIES = 'categories';
-  static const String _OPERATIONS = 'operations';
-  static const String _USERS = 'users';
-
-  DocumentReference<Map<String, dynamic>>? _db;
-  bool _isCurrentAdmin = false;
+  CloudDb? _cloudDb;
 
   RemoteSourceImpl(this._firestore);
 
   @override
-  TableDAO<CloudAccount>? get accounts => _db != null
-      ? AccountsDAO(
-          collection: _db!.collection(_ACCOUNTS),
-          key_updated: AccountMapper.KEY_UPDATED,
-          mapper: const AccountMapper(),
-        )
-      : null;
+  TableDAO<CloudAccount>? get accounts => _cloudDb?.accounts;
 
   @override
-  TableDAO<CloudCategory>? get categories => _db != null
-      ? CategoriesDAO(
-          collection: _db!.collection(_CATEGORIES),
-          key_updated: CategoryMapper.KEY_UPDATED,
-          mapper: const CategoryMapper(),
-        )
-      : null;
+  TableDAO<CloudCategory>? get categories => _cloudDb?.categories;
 
   @override
-  TableDAO<CloudOperation>? get operations => _db != null
-      ? OperationDAO(
-          collection: _db!.collection(_OPERATIONS),
-          key_updated: OperationMapper.KEY_UPDATED,
-          mapper: const OperationMapper(),
-        )
-      : null;
+  TableDAO<CloudOperation>? get operations => _cloudDb?.operations;
 
-  /// Throws [NetworkException] and [NoRemoteDBException]
-  Future<DocumentReference<Map<String, dynamic>>> _getDatabase(
-    String userId,
-  ) async {
-    QuerySnapshot querySnapshot;
-    querySnapshot = await _firestore
-        .collection(_DATABASES)
-        .where(_DATABASES_USERS, arrayContains: userId)
-        .get();
-
-    if (querySnapshot.docs.isNotEmpty) {
-      return _firestore.collection(_DATABASES).doc(querySnapshot.docs.first.id);
-    } else {
-      throw NoRemoteDBException();
-    }
-  }
-
-  ///Throws [NoRemoteDBException] and [NetworkException]
   @override
   Future<void> deleteAll() async {
-    if (_db == null) {
-      throw NoRemoteDBException();
-    }
-
-    try {
-      await operations?.deleteAll();
-      await categories?.deleteAll();
-      await accounts?.deleteAll();
-    } on NoRemoteDBException {
-      rethrow;
-    } on NetworkException {
-      rethrow;
-    }
+    await operations?.deleteAll();
+    await categories?.deleteAll();
+    await accounts?.deleteAll();
   }
 
-  ///Throws [NoRemoteDBException] and [NetworkException]
   @override
-  Future<void> addUserToDatabase(User user) async {
-    if (_db != null) {
-      throw NoRemoteDBException();
-    }
+  Future<void> addUserToDatabase(User user) =>
+      _cloudDb?.addUserToDatabase(user) ?? Future(() => {});
 
-    var doc = await _db!.get();
-    var data = doc.data();
-    data![_DATABASES_USERS].add(user.id);
-    await _db!.set(data);
-
-    await _db!
-        .collection(_USERS)
-        .doc(user.id)
-        .set(const UserMapper().mapToCloud(user));
-  }
-
-  /// Throws [NoRemoteDBException] and [NetworkException]
   @override
-  Future<List<User>> getAllUsers() async {
-    if (_db == null) {
-      throw NoRemoteDBException();
-    }
+  Future<List<User>> getAllUsers() =>
+      _cloudDb?.getAllUsers() ?? Future.value([]);
 
-    var doc = await _db!.collection(_USERS).get();
-
-    return doc.docs.map((doc) => const UserMapper().mapToDart(doc)).toList();
-  }
-
-  /// Throws [NetworkException]
   @override
   Future<void> createDatabase(User user) async {
-    _db = await _firestore.collection(_DATABASES).add({
-      _DATABASES_ADMIN: user.id,
-      _DATABASES_USERS: [user.id],
-    });
-    await _db!
-        .collection(_USERS)
-        .doc(user.id)
-        .set(const UserMapper().mapToCloud(user));
+    _cloudDb = await CloudDb.create(_firestore, user);
   }
 
-  /// Throws [NetworkException]
   @override
-  Future<bool> databaseExists(User user) async {
-    QuerySnapshot querySnapshot = await _firestore
-        .collection(_DATABASES)
-        .where(_DATABASES_USERS, arrayContains: user.id)
-        .get();
+  Future<bool> databaseExists(User user) =>
+      CloudDb.databaseExists(_firestore, user);
 
-    return querySnapshot.docs.isNotEmpty;
-  }
-
-  /// Throws [NoRemoteDBException] and [NetworkException]
   @override
   Future<void> connect(User user) async {
-    try {
-      _db = await _getDatabase(user.id);
-      var doc = await _db!.get();
-
-      _isCurrentAdmin = user.id == doc.data()![_DATABASES_ADMIN];
-    } on NoRemoteDBException {
-      _db = null;
-      _isCurrentAdmin = false;
-      rethrow;
-    } on NetworkException {
-      _db = null;
-      _isCurrentAdmin = false;
-      rethrow;
-    } on Object catch (e, stackTrace) {
-      _db = null;
-      _isCurrentAdmin = false;
-    }
+    _cloudDb = await CloudDb.findByUserId(_firestore, user.id);
   }
 
   @override
   Future<void> disconnect() async {
-    _db = null;
-    _isCurrentAdmin = false;
+    _cloudDb = null;
   }
 
-  /// Throws [NoRemoteDBException] and [NetworkException]
   @override
-  bool isCurrentAdmin() {
-    if (_db == null) {
-      throw NoRemoteDBException();
-    }
-
-    return _isCurrentAdmin;
-  }
+  bool isCurrentAdmin() => _cloudDb?.isCurrentUserAdmin ?? false;
 }
