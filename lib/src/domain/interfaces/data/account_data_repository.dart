cd33f44e4df56e8804239401a63f8ed2d651b27f
Pavel <epipasha@gmail.com>
2024-01-31 15:19:24 +0300

    Fixes

--- lib/src/domain/interfaces/data/account_data_repository.dart
+++ lib/src/domain/interfaces/data/account_data_repository.dart
@@ -1,19 +1,16 @@
 import '../../models.dart';
 
 abstract class AccountDataRepository {
-  Stream<List<Account>> watchAll();
 
+  Stream<List<Account>> watchAll();
   Future<List<Account>> getAll();
 
   Stream<List<AccountBalance>> watchAllBalance();
-
   Future<List<AccountBalance>> getAllBalance();
 
   Stream<Account> watchById(int id);
-
   Future<Account> getById(int id);
 
   Future<int> insert(Account account);
-
   Future<void> update(Account account);
 }
