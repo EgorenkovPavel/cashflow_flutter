875858e6b9edcb8724c127ec112d4584feb4ce18
Pavel <epipasha@gmail.com>
2025-04-03 12:31:41 +0300

    Bug fix

--- lib/src/ui/widgets/currency_menu.dart
+++ lib/src/ui/widgets/currency_menu.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../domain/models.dart';
 
-class CurrencyMenu extends StatefulWidget {
+class CurrencyMenu extends StatelessWidget {
   final Currency currency;
   final void Function(Currency) onChange;
 
@@ -9,23 +9,10 @@ class CurrencyMenu extends StatefulWidget {
       {super.key, required this.currency, required this.onChange});
 
   @override
-  State<CurrencyMenu> createState() => _CurrencyMenuState();
-}
-
-class _CurrencyMenuState extends State<CurrencyMenu> {
-  late Currency _currency;
-
-  @override
-  void initState() {
-    super.initState();
-    _currency = widget.currency;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return DropdownButton<Currency>(
       underline: SizedBox(),
-        value: _currency,
+        value: currency,
         items: Currency.values
             .map((e) => DropdownMenuItem<Currency>(
                   value: e,
@@ -36,10 +23,7 @@ class _CurrencyMenuState extends State<CurrencyMenu> {
           if (value == null) {
             return;
           }
-          widget.onChange(value);
-          setState(() {
-            _currency = value;
-          });
+          onChange(value);
         });
   }
 }
