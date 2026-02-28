8e12b06e1f5adc04c5a34dfb4399a42c74e2a82f
Pavel <epipasha@gmail.com>
2026-02-28 12:15:09 +0300

    Refactoring

--- lib/src/domain/interactors/currency_interactor.dart
+++ lib/src/domain/interactors/currency_interactor.dart
@@ -1,4 +1,4 @@
-import 'package:money_tracker/src/domain/interfaces/currency_repository.dart';
+import '../interfaces/currency_repository.dart';
 import '../models.dart';
 
 class CurrencyInteractor {
