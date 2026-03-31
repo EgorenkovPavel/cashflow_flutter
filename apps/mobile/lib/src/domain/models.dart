b5173c8d678bbd312663f548c94478b087fec4a6
Pavel <epipasha@gmail.com>
2026-03-31 12:26:45 +0300

    Refactoring

--- apps/mobile/lib/src/domain/models.dart
+++ apps/mobile/lib/src/domain/models.dart
@@ -1,10 +1,12 @@
 import 'models.dart';
 
-export 'package:finance_models/finance_models.dart';
-
+export 'models/account/account.dart';
+export 'models/category/category.dart';
+export 'models/category/category_cashflow.dart';
+export 'models/operation/operation.dart';
 export 'models/operation/operation_list_filter.dart';
 export 'models/google_drive_file.dart';
 export 'models/user.dart';
-
+export 'package:finance_models/finance_models.dart';
 
 Currency baseCurrency = Currency.RUB;
--- packages/finance_models/lib/finance_models.dart
+++ packages/finance_models/lib/finance_models.dart
@@ -3,10 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/account/account.dart';
-export 'src/category/category.dart';
-export 'src/category/category_cashflow.dart';
-export 'src/operation/operation.dart';
 export 'src/enum/budget_type.dart';
 export 'src/enum/category_type.dart';
 export 'src/enum/currency.dart';
